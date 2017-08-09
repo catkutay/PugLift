037b33b235206298ebd25f5788374e42a82574f1
Alex Dacre <alex@alexdacre.com>
2017-08-09 22:27:18 +1000

    Created a basic server that can handle multiple requests at the same time

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const http = require('uws').http
+
+const document = Buffer.from('Hello world!')
+
+const server = http.createServer((req, res) => {
+  // handle some POST data
+  if (req.method === 'POST') {
+    let body = []
+    req.on('data', (chunk) => {
+      body.push(Buffer.from(chunk))
+    }).on('end', () => {
+      res.end('You posted me this: ' + Buffer.concat(body).toString())
+    })
+    // handle some GET url
+  } else if (req.url === '/') {
+    res.end(document)
+  } else {
+    res.end('Unknown request by: ' + req.headers['user-agent'])
+  }
+})
+
+server.listen(3000)

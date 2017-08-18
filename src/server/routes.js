92aea004afb5130e708282729aee23da0495fae2
Alex Dacre <a.dacre@thinkbits.com>
2017-08-18 17:13:14 +1000

    changed routes to be more relevant to our project

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,18 +1,17 @@
-const document = Buffer.from('Hello world!')
+const home = Buffer.from('Hello world!')
+const page = Buffer.from('page loaded!')
+const reqs = Buffer.from('bid requests!')
+const results = Buffer.from('bid results!')
+const creative = Buffer.from('creative render!')
 
 const handleRoutes = (req, res) => {
-  if (req.method === 'POST') {
-    let body = []
-    req.on('data', (chunk) => {
-      body.push(Buffer.from(chunk))
-    }).on('end', () => {
-      res.end(`You posted me this: ${Buffer.concat(body).toString()}`)
-    })
-    // handle some GET url
-  } else if (req.url === '/') {
-    res.end(document)
-  } else {
-    res.end(`Unknown request by: ${req.headers['user-agent']}`)
+  switch (req.url) {
+    case '/': return res.end(home)
+    case '/page_load': return res.end(page)
+    case '/bid_requests': return res.end(reqs)
+    case '/bid_results': return res.end(results)
+    case '/creative_render': return res.end(creative)
+    default: res.end(`Unknown request by: ${req.headers['user-agent']}`)
   }
 }
 

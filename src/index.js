cb4741c177db990912de44b6edfa7139a0ee1abd
Alex Dacre <a.dacre@thinkbits.com>
2017-08-22 11:10:08 +1000

    changed server connection

--- src/index.js
+++ src/index.js
@@ -30,10 +30,21 @@ import handleRoutes, { PORT } from './server/routes'
 
 const server = new WebSocket.Server({ port: PORT })
 
+const home = Buffer.from('Hello world!')
+const page = Buffer.from('page loaded!')
+const reqs = Buffer.from('bid requests!')
+const results = Buffer.from('bid results!')
+const creative = Buffer.from('creative render!')
+
 server.on('connection', ws => {
   ws.on('message', message => {
-    console.log(`received: ${message}`)
-
-    ws.send('something')
+    switch (message.type) {
+      case '': return ws.send(home)
+      case 'page_load': return ws.send(page)
+      case 'bid_requests': return ws.send(reqs)
+      case 'bid_results': return ws.send(results)
+      case 'creative_render': return ws.send(creative)
+      default: ws.send(`Unknown request by: ${ws}`)
+    }
   })
 })

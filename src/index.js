cdcd25cc1677df8b2cadd6555bb5373756b8a164
Alex Dacre <a.dacre@thinkbits.com>
2017-08-22 14:51:40 +1000

    cleared unneeded dependencies for this branch and added rethinkdb

--- src/index.js
+++ src/index.js
@@ -1,34 +1,4 @@
-/* eslint-disable no-unused-vars */
-
-// import primus from './server/primus'
-//
-// primus.on('initialised', spark => {
-//   console.log('Primus initialised')
-//
-//   spark.on('data', data => {
-//     console.log(data)
-//
-//     spark.write(`${data}? That's awesome`)
-//   })
-// })
-//
-// primus.on('connection', spark => {
-//   console.log('hello connection')
-//
-//   spark.on('data', data => {
-//     console.log(data)
-//
-//     spark.write(`${data}? That's awesome`)
-//   })
-// })
-
-import WebSocket from 'uws'
-import http from './server/uws'
-import handleRoutes, { PORT } from './server/routes'
-
-// const app = http.createServer(handleRoutes)
-
-const server = new WebSocket.Server({ port: PORT })
+import server from './server/uws'
 
 const home = Buffer.from('Hello world!')
 const page = Buffer.from('page loaded!')
@@ -47,4 +17,8 @@ server.on('connection', ws => {
       default: ws.send(`Unknown request by: ${ws}`)
     }
   })
+})
+
+server.on('listening', ws => {
+  console.info('PubLift Analytics server started and listening for connections')
 })
--- src/server/primus.js
+++
@@ -1,9 +0,0 @@
-import Primus from 'primus'
-import WebSocket from 'uws'
-import { PORT } from './routes'
-
-const server = new WebSocket.Server({ port: PORT })
-
-const primus = new Primus(server, { transformer: 'uws' })
-
-export default primus

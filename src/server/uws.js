81f58b410b2cc2bdb0ab0b98466b694b91d15923
Alex Dacre <a.dacre@thinkbits.com>
2017-09-15 09:42:03 +1000

    seperated websocket server and httpserver blocks

--- src/server/uws.js
+++ src/server/uws.js
@@ -3,12 +3,10 @@ import { PORT } from './routes'
 
 const uws = routes => {
   const httpServer = http.createServer((req, res) => routes.handleRoutes(req, res))
-  const server = new WebSocket.Server({ port: 65080 })
-
   httpServer.listen(PORT)
 
+  const server = new WebSocket.Server({ port: 65080 })
   server.on('connection', ws => routes.handleWebsocketRoutes(ws))
-
   server.on('listening', () => {
     console.info('PubLift Analytics server started and listening for connections')
   })

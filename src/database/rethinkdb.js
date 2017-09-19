ee98be0e5e41c183f452085bc4e21cf63b4ad8d2
Alex Dacre <a.dacre@thinkbits.com>
2017-09-19 10:06:54 +1000

    reconnected to rethinkdb for tests

--- src/database/rethinkdb.js
+++ src/database/rethinkdb.js
@@ -2,31 +2,31 @@ import rethinkdb from 'rethinkdb'
 import { response } from '../server/routes'
 
 export let connection = null
-//
-// const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? '10.152.0.2' : 'localhost'
-//
-// const createDatabase = conn => {
-//   rethinkdb.dbList()
-//     .contains('publift')
-//     .do(exists => rethinkdb.branch(exists, {databases_created: 0}, rethinkdb.dbCreate('publift')))
-//     .run(conn).then(createTables(conn))
-// }
-//
-// const createTables = conn => {
-//   conn.use('publift')
-//   rethinkdb.tableList()
-//     .contains('session')
-//     .do(exists => rethinkdb.branch(exists, {tables_created: 0}, rethinkdb.tableCreate('session')))
-//     .run(conn)
-//   connection = conn
-// }
-//
-// rethinkdb.connect({ host: HOST, port: 28015 })
-//   .then(createDatabase)
-//   .catch(error => {
-//     throw error
-//   })
-//
+
+const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? '10.152.0.2' : 'localhost'
+
+const createDatabase = conn => {
+  rethinkdb.dbList()
+    .contains('publift')
+    .do(exists => rethinkdb.branch(exists, {databases_created: 0}, rethinkdb.dbCreate('publift')))
+    .run(conn).then(createTables(conn))
+}
+
+const createTables = conn => {
+  conn.use('publift')
+  rethinkdb.tableList()
+    .contains('session')
+    .do(exists => rethinkdb.branch(exists, {tables_created: 0}, rethinkdb.tableCreate('session')))
+    .run(conn)
+  connection = conn
+}
+
+rethinkdb.connect({ host: HOST, port: 28015 })
+  .then(createDatabase)
+  .catch(error => {
+    throw error
+  })
+
 export function handleEvent (data, callback) {
   rethinkdb.table('session')
     .insert(data.value)
@@ -38,5 +38,5 @@ export function handleEvent (data, callback) {
       throw err
     })
 }
-//
-// export default rethinkdb
+
+export default rethinkdb
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,5 +1,5 @@
-// import fs from 'fs'
-// import path from 'path'
+import fs from 'fs'
+import path from 'path'
 import * as pkg from '../../package.json'
 
 class Routes {
@@ -8,48 +8,48 @@ class Routes {
   }
 
   handleRoutes (req, res) {
-    if (req.url === '/') {
-      return res.end(
-        Buffer.from(
-          `Running Publift Analytics v${pkg.version}${pkg.config.branch === 'master' ? '' : ' from branch ' + pkg.config.branch}`
-        )
-      )
+    // if (req.url === '/') {
+    //   return res.end(
+    //     Buffer.from(
+    //       `Running Publift Analytics v${pkg.version}${pkg.config.branch === 'master' ? '' : ' from branch ' + pkg.config.branch}`
+    //     )
+    //   )
+    // } else {
+    //   res.statusCode = 200
+    //   return res.end('success')
+    // }
+
+    if (req.method === 'POST') {
+      let bodyData = ''
+      req.on('data', data => { bodyData += ab2str(data) })
+      req.on('end', () => {
+        const event = JSON.parse(bodyData)
+        if (Object.keys(response).includes(req.url.substr(1))) {
+          return this.handleEvent(event, response => res.end(response))
+        } else {
+          return res.end(`Unknown request by: ${req.headers['user-agent']}`)
+        }
+      })
     } else {
-      res.statusCode = 200
-      return res.end('success')
+      if (req.url === '/') {
+        return res.end(
+          Buffer.from(
+            `Running Publift Analytics v${pkg.version}${pkg.config.branch === 'master' ? '' : ' from branch ' + pkg.config.branch}`
+          )
+        )
+      } else if (req.url === '/loaderio-af0883d94320b9a9907450652cbd426c') {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain',
+        })
+
+        const readStream = fs.createReadStream(path.join(__dirname, '/public/verification_file'))
+        return readStream.pipe(res)
+      } else {
+        return res.end(Buffer.from(`404 - Unknown request by: ${req.headers['user-agent']}`))
+      }
     }
 
-    // if (req.method === 'POST') {
-    //   let bodyData = ''
-    //   req.on('data', data => { bodyData += ab2str(data) })
-    //   req.on('end', () => {
-    //     const event = JSON.parse(bodyData)
-    //     if (Object.keys(response).includes(req.url.substr(1))) {
-    //       return this.handleEvent(event, response => res.end(response))
-    //     } else {
-    //       return res.end(`Unknown request by: ${req.headers['user-agent']}`)
-    //     }
-    //   })
-    // } else {
-    //   if (req.url === '/') {
-    //     return res.end(
-    //       Buffer.from(
-    //         `Running Publift Analytics v${pkg.version}${pkg.config.branch === 'master' ? '' : ' from branch ' + pkg.config.branch}`
-    //       )
-    //     )
-    //   } else if (req.url === '/loaderio-af0883d94320b9a9907450652cbd426c') {
-    //     res.writeHead(200, {
-    //       'Content-Type': 'text/plain',
-    //     })
-    //
-    //     const readStream = fs.createReadStream(path.join(__dirname, '/public/verification_file'))
-    //     return readStream.pipe(res)
-    //   } else {
-    //     return res.end(Buffer.from(`404 - Unknown request by: ${req.headers['user-agent']}`))
-    //   }
-    // }
-    //
-    // const ab2str = buf => String.fromCharCode.apply(null, new Uint8Array(buf))
+    const ab2str = buf => String.fromCharCode.apply(null, new Uint8Array(buf))
   }
 
   // handleWebsocketRoutes (ws) {

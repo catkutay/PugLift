06d3060d2b40954d6811877de00efaa761aca84b
Cattleya Tantri <cattleya.tantri@student.uts.edu.au>
2017-09-19 20:45:09 +1000

    rethinkdb.js: creating separate tables for each event and converting timestamps routes.js: added to response keys

--- src/database/rethinkdb.js
+++ src/database/rethinkdb.js
@@ -5,6 +5,15 @@ export let connection = null
 
 const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? '10.152.0.2' : 'localhost'
 
+const tables = [
+  'page_load',
+  'bid_request',
+  'bid_response',
+  'ad_request',
+  'ad_response',
+  'ad_view',
+]
+
 const createDatabase = conn => {
   rethinkdb.dbList()
     .contains('publift')
@@ -14,10 +23,16 @@ const createDatabase = conn => {
 
 const createTables = conn => {
   conn.use('publift')
+
+  // get list of existing tables and if the tables names above do not already exist, create them
   rethinkdb.tableList()
-    .contains('session')
-    .do(exists => rethinkdb.branch(exists, {tables_created: 0}, rethinkdb.tableCreate('session')))
     .run(conn)
+    .then(list => {
+      tables.forEach(table => {
+        rethinkdb.branch(list.includes(table), {tables_created: 0}, rethinkdb.tableCreate(table))
+          .run(conn)
+      })
+    })
   connection = conn
 }
 
@@ -28,8 +43,10 @@ rethinkdb.connect({ host: HOST, port: 28015 })
   })
 
 export function handleEvent (data, callback) {
-  rethinkdb.table('session')
-    .insert(data.value)
+  // convert timestamps to date objects
+  data.timestamp = rethinkdb.epochTime(data.timestamp)
+  rethinkdb.table(data.event_label)
+    .insert(data)
     .run(connection)
     .then(result => {
       callback(response[data.type])

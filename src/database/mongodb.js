47122017f0d094aceecabbddd7aae783dbb6604d
Kory <kory.porter@hotmail.com>
2017-10-17 11:29:57 +1100

    Added mongodb back to project, also added some code to check if the "type" of JSON data exists in the 'response' object.

---
+++ src/database/mongodb.js
@@ -0,0 +1,18 @@
+import { response } from '../server/routes'
+import { MongoClient } from 'mongodb'
+
+const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? 'TBD' : 'localhost'
+const url = `mongodb://${HOST}:27017/publift`
+const mongo = new MongoClient()
+let conn = null
+
+mongo.connect(url, (err, db) => {
+  if (err) { console.log(err) }
+  conn = db
+})
+
+export function handleEvent (data, callback) {
+  const coll = conn.collection(data.type)
+  coll.insertOne(data.value)
+  callback(response[data.type])
+}

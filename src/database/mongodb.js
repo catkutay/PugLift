b3796c900a8d09c4a4081c9420c9a73aef633b7b
Kory <kory.porter@hotmail.com>
2017-10-18 17:57:41 +1100

    fixed stupid mistake

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,7 +1,7 @@
 import { response } from '../server/routes'
 import { MongoClient } from 'mongodb'
 
-const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? '10.152.0.11' : 'localhost'
+const HOST = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? '10.152.0.12' : 'localhost'
 const url = `mongodb://${HOST}:27017/publift`
 const mongo = new MongoClient()
 let conn = null
@@ -12,7 +12,7 @@ mongo.connect(url, (err, db) => {
 })
 
 export function handleEvent (data, callback) {
-  const coll = conn.collection(data.type)
-  coll.insertOne(data.value)
+  const collection = conn.collection(data.type)
+  collection.insertOne(data.value)
   callback(response[data.type])
 }

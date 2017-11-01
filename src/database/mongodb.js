0894aac422c07f2d5ff1c9646db2572238bf199e
Cattleya Tantri <cattleya.tantri@student.uts.edu.au>
2017-11-01 15:23:16 +1100

    Added convertDate() to convert event timestamps

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -12,6 +12,7 @@ mongo.connect(url, (err, db) => {
 })
 
 export function handleEvent (data, callback) {
+  convertDate(data)
   const collection = conn.collection(data.type)
   if (Array.isArray(data.value)) {
     collection.insertMany(data.value) // allow for the insertion of arrays of JSON (specifically for ad_request)
@@ -19,4 +20,32 @@ export function handleEvent (data, callback) {
     collection.insertOne(data.value)
   }
   callback(response[data.type])
+}
+
+function convertDate (parsedObj) {
+  // convert event timestamps to ISO strings recursively (given objects with multiple nesting levels) so that the result can be manipulated for the purposes of the query API
+  // code reference: https://stackoverflow.com/questions/15523514/find-by-key-deep-in-a-nested-object
+  let nestedObj = null
+  if (parsedObj instanceof Array) {
+    for (let i = 0; i < parsedObj.length; i++) {
+      nestedObj = convertDate(parsedObj[i])
+      if (nestedObj) {
+        break
+      }
+    }
+  } else {
+    for (let prop in parsedObj) {
+      // find timestamps by value format rather than key; assumes the format is YYYY-MM-DDT00:00:00.000Z
+      if (/^[\d]{4}-[\d]{2}-[\d]{1,2}T[\d]{2}:[\d]{2}:[\d]{2}\.[\d]{3}Z$/.test(parsedObj[prop])) {
+        parsedObj[prop] = new Date(parsedObj[prop]).toISOString()
+      }
+      if (parsedObj[prop] instanceof Object || parsedObj[prop] instanceof Array) {
+        nestedObj = convertDate(parsedObj[prop])
+        if (nestedObj) {
+          break
+        }
+      }
+    }
+  }
+  return nestedObj
 }
